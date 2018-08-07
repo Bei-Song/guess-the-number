96dd1d4b6c971b312b3a5522d83ab0f086504180
Bei Song <vincent17.lz12@gmail.com>
2018-08-07 15:39:04 +0800

    lotto2.0

--- lotto.py
+++ lotto.py
@@ -1,16 +1,18 @@
 #lotto
 print ('請猜一個數字1~100之間')
 import random
-
+count = 0
 r = random.randint(1, 100)
-r = int(r)
 while True:
 	y = input ('請猜一個數字：')
 	y = int(y)
+	count = count + 1
 	if r == y:
 		print ('答對了')
+		print ('您總共猜了' ,count, '次')
 		break
 	elif r > y:
 		print ('猜太小了')
 	elif r < y:
 		print ('猜太大嚕')
+	print ('您總共猜了' ,count, '次')
